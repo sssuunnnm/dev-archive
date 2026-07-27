eced6a42a1f2b44f8854a1eddabb7365462c50e8
sssuunnnm <63292816+sssuunnnm@users.noreply.github.com>
2026-07-27 15:57:00 +0900

    chore: astro.config, github actions 배포 설정 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sssuunnnm.github.io',   // GitHub 사용자명
+  base: '/dev-archive',                    // 레포명
   vite: {
     plugins: [tailwindcss()]
   }
